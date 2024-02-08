3d92bf53839da7d1a804920a732e829348c5dafd
Njsa13 <najib.sauqi@students.amikom.ac.id>
2024-02-08 16:01:48 +0700

    Initial commit

---
+++ src/main/java/com/backend/seabook/SeabookApplication.java
@@ -0,0 +1,13 @@
+package com.backend.seabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeabookApplication.class, args);
+	}
+
+}
